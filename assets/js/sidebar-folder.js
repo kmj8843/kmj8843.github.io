dd78c5a93b830837e33f42c1737784e74fa30d23
김민재_이모션 <minjae.kim@emotion.co.kr>
2023-05-18 10:59:06 +0900

    사이드바 클릭 이벤트 수정

--- assets/js/sidebar-folder.js
+++ assets/js/sidebar-folder.js
@@ -1,4 +1,8 @@
 function spread(count){
+    document.querySelectorAll('[id^=folder-checkbox-]:not([id=folder-checkbox-' + count +'])').forEach(n => n.checked = false);
+    document.querySelectorAll('[id^=spread-icon-]:not([id=spread-icon-' + count +'])').forEach(n => n.innerHTML = 'arrow_right');
+
+    
     document.getElementById('folder-checkbox-' + count).checked =
     !document.getElementById('folder-checkbox-' + count).checked
     document.getElementById('spread-icon-' + count).innerHTML =
