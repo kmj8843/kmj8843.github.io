22c8931ca601ac918822a3e75ae9d547cb7c6256
김민재_이모션 <minjae.kim@emotion.co.kr>
2023-05-18 11:11:28 +0900

    사이드바 길어질 시 소셜바 숨김처리 추가

--- assets/js/sidebar-folder.js
+++ assets/js/sidebar-folder.js
@@ -8,4 +8,9 @@ function spread(count){
     document.getElementById('spread-icon-' + count).innerHTML =
     document.getElementById('spread-icon-' + count).innerHTML == 'arrow_right' ?
     'arrow_drop_down' : 'arrow_right'
+
+    var clientHeight = document.documentElement.clientHeight;
+    var nodeHeight = document.querySelector('.sidebar-nav.heading').offsetHeight;
+
+    document.querySelector('.sidebar-sticky .sidebar-social').style.display = clientHeight / 2 > nodeHeight ? 'block' : 'none';
 }
